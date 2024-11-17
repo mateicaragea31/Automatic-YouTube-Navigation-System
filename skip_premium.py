85d16d0f36a2888c98672ec76ad54147736daf19
Matei-Ioan Caragea <106037161+mateicaragea31@users.noreply.github.com>
2024-11-18 00:07:48 +0200

    Update skip_premium.py

--- skip_premium.py
+++ skip_premium.py
@@ -1,15 +1,17 @@
-# importarea librariilor necesare
+import time
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from logging_config import log_message, log_error, log_warning, log_debug
 
 def skip_premium_popup(driver):
     try:
-        # asteapta ca butonul sa fie vizibil si-l apasa
+        # asteapta ca butonul 'no thanks' sa fie vizibil si-l apasa
         no_thanks_button = WebDriverWait(driver, 5).until(
             EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "No thanks")]'))
         )
         no_thanks_button.click()
-        print("Popup-ul pentru youtube premium a fost omis.")
+        log_message("Popup-ul pentru YouTube Premium a fost omis.")
     except Exception as e:
-        print("Nu a fost gasit butonul 'nu multumesc':", e)
+        log_warning(f"Nu a fost gasit butonul 'No thanks' pentru popup-ul YouTube Premium: {e}")
