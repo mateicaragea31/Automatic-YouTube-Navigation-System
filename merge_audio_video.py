62d835db58e01147137c11d4fd2ff6c3b539213e
Matei-Ioan Caragea <106037161+mateicaragea31@users.noreply.github.com>
2024-11-18 00:07:03 +0200

    Update merge_audio_video.py

--- merge_audio_video.py
+++ merge_audio_video.py
@@ -1,14 +1,20 @@
-#importarea librariilor necesare
 import ffmpeg
+from logging_config import log_message, log_error
 
 def merge_audio_video(video_file, audio_file, output_file):
     try:
-        # input video si audio separat cu 'analyzeduration' si 'probesize' crescute?
-        video = ffmpeg.input(video_file, analyzeduration=2147483647, probesize=2147483647)
-        audio = ffmpeg.input(audio_file)
+        # deschide fișierele video și audio ca fluxuri de input
+        video_input = ffmpeg.input(video_file)
+        audio_input = ffmpeg.input(audio_file)
 
-        # re-encodeaza video-ul intr-un format standard cu codec h.264 si audio aac
-        ffmpeg.output(video, audio, output_file, vcodec='libx264', acodec='aac', strict='experimental').run()
-        print(f"Imbinarea si re-encodarea audio si video s-au finalizat cu succes in {output_file}")
-    except ffmpeg.Error as e:
-        print(f"A aparut o eroare la imbinarea si re-encodarea audio si video: {e}")
+        # configurare pentru îmbinarea audio și video
+        (ffmpeg.output(video_input, audio_input, output_file,
+                      vcodec='copy',
+                      acodec='aac',
+                      strict='experimental')
+         .run(overwrite_output=True))
+
+        log_message(f"Fișierele {video_file} și {audio_file} au fost combinate cu succes în {output_file}")
+
+    except Exception as e:
+        log_error(f"Eroare la imbinarea fisierelor audio si video: {e}")
