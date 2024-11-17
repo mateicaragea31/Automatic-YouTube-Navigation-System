bc74659d6731d371ce6a2078fc92414d9346b523
Matei-Ioan Caragea <106037161+mateicaragea31@users.noreply.github.com>
2024-11-18 00:04:39 +0200

    Update audio_recording.py

--- audio_recording.py
+++ audio_recording.py
@@ -1,53 +1,21 @@
-#importarea librariilor necesare
-import pyaudio
-import wave
-import time
+import sounddevice as sd
+import soundfile as sf
+from logging_config import log_message, log_error, log_debug
 
-def record_audio(output_filename="audio_record.wav", sample_rate=44100, chunk_size=1024, duration=15):
-    # setari audio
-    FORMAT = pyaudio.paInt16  # formatul esantionului
-    channels = 1  # mono
-    frames = []
+def audio_recording(output_file, duration, sample_rate, device_index):
+    try:
+        log_debug(f"Inceperea inregistrarii audio de pe dispozitivul cu indexul {device_index}...")
 
-    # initializeaza obiectul pyaudio
-    p = pyaudio.PyAudio()
+        # porneste inregistrarea audio de pe dispozitivul selectat
+        audio_data = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=2, device=device_index)
 
-    # deschide stream-ul ca input si output
-    stream = p.open(format=FORMAT,
-                    channels=channels,
-                    rate=sample_rate,
-                    input=True,
-                    output=True,
-                    frames_per_buffer=chunk_size)
+        # asteapta terminarea inregistrarii
+        sd.wait()
 
-    print(f"Inregistrare audio pentru {duration} secunde...")
+        # salveaza inregistrarea audio in fisierul wav
+        sf.write(output_file, audio_data, sample_rate)
 
-    start_time = time.time()  # timpul de inceput pentru inregistrare
+        log_message(f"Fisierul audio a fost salvat in {output_file}")
 
-    while True:
-        # citeste date audio din stream
-        data = stream.read(chunk_size)
-
-        # adauga datele in lista de frame-uri
-        frames.append(data)
-
-        # opreste inregistrarea dupa durata specificata
-        if time.time() - start_time >= duration:
-            print("Inregistrare oprita dupa durata specificata.")
-            break
-
-    # opreste si inchide stream-ul
-    stream.stop_stream()
-    stream.close()
-
-    # termina obiectul pyaudio
-    p.terminate()
-
-    # salveaza inregistrarea audio in fisierul de iesire
-    with wave.open(output_filename, "wb") as wf:
-        wf.setnchannels(channels)
-        wf.setsampwidth(p.get_sample_size(FORMAT))
-        wf.setframerate(sample_rate)
-        wf.writeframes(b"".join(frames))
-
-    print(f"Inregistrarea a fost salvata in {output_filename}")
+    except Exception as e:
+        log_error(f"Eroare in timpul inregistrarii audio: {e}")
