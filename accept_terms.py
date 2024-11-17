4ba6d11a4687907082f9993a95440af1e2fe80da
Matei-Ioan Caragea <106037161+mateicaragea31@users.noreply.github.com>
2024-11-18 00:03:54 +0200

    Update accept_terms.py

--- accept_terms.py
+++ accept_terms.py
@@ -1,15 +1,16 @@
-#importarea librariilor necesare
+# importarea librariilor necesare
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from logging_config import log_message, log_error, log_warning, log_debug
 
 def reject_terms(driver):
     try:
-        # asteapta ca butonul 'Reject all' sa devina activabil si da click pe el
+        # asteapta ca butonul 'reject all' sa devina activabil si da click pe el
         button = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, "//button[.//span[text()='Reject all']]"))
         )
         button.click()
-        print("Popup respins cu succes.")
+        log_message("Popup-ul pentru termeni si conditii a fost respins cu succes.")
     except Exception as e:
-        print("Nu s-a putut gasi butonul 'Reject all':", e)
+        log_warning(f"Nu s-a putut gasi butonul 'Reject all' pentru termeni si conditii: {e}")
